d091bfceae1e0a6cd8651e708f82ab3f251e3c6e
sarveshhome <sarvesh.carlton@gmail.com>
2021-05-31 20:30:18 +0100

    React expo audio app

---
+++ App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TouchableOpacity, Text, View } from 'react-native';
+import styles from './styles';
+import AudioSlider from './src/AudioSlider';
+import AudioFile from './assets/counting.m4a';
+
+
+export default function App() {
+  return (
+
+        <View style={[styles.StandardContainer, {
+                flex: 0,
+                flexDirection: "column",
+                justifyContent: "flex-start",
+                marginTop: 100,
+                marginBottom: 5,
+            }]}>
+
+            <View style={{
+                flex: 0,
+                flexDirection: "row",
+                justifyContent: "space-between",
+                }}>
+
+                <Text style={[styles.StandardText, {flex: 5}]}>
+                    Audio name
+                </Text>
+
+                <TouchableOpacity 
+                    style={[{flex: 1}]}
+                >
+                    <Text style={styles.StandardText}>Edit</Text>
+                </TouchableOpacity>
+                
+
+            </View>
+
+            <AudioSlider audio={AudioFile}/>
+
+        </View>
+  );
+}
